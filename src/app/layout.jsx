69d2869e8505d4712caeecb2d9585a62571a8893
Zainul <thobazainul@gmail.com>
2024-11-19 01:03:33 +0700

    fix again

---
+++ src/app/layout.jsx
@@ -0,0 +1,25 @@
+import { Poppins } from 'next/font/google';
+import './globals.css';
+import 'react-toastify/dist/ReactToastify.css';
+
+const poppins = Poppins({
+  subsets: ['latin'],
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+});
+
+export const metadata = {
+  title: 'GeoRescue',
+  description: 'This created by Zainul',
+  authors: [{ name: 'Bigboss Company' }, { url: 'https://georescue.vercel.app/' }],
+  icons: {
+    icon: '/logo.jpeg',
+  },
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={poppins.className}>{children}</body>
+    </html>
+  );
+}
