6333e41601bd772233594856668667f57c456adb
zainuull <thobazainul@gmail.com>
2024-12-18 06:57:09 +0700

    fix

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,44 +1,44 @@
 'use client';
-// import dynamic from 'next/dynamic';
-// import ToastNotify from '@/core/services/notify/toast';
+import dynamic from 'next/dynamic';
+import ToastNotify from '@/core/services/notify/toast';
 import './globals.css';
-// import { useEffect, useState } from 'react';
-// import axios from 'axios';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
-// const Map3D = dynamic(() => import('./(components)/map3d'), { ssr: false });
+const Map3D = dynamic(() => import('./(components)/map3d'), { ssr: false });
 
-// const headers = {
-//   'Content-Type': 'application/json',
-//   Accept: 'application/json',
-// };
+const headers = {
+  'Content-Type': 'application/json',
+  Accept: 'application/json',
+};
 
-// const instance = axios.create({
-//   baseURL: process.env.NEXT_PUBLIC_BASE_API_URL,
-//   headers: headers,
-// });
+const instance = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_BASE_API_URL,
+  headers: headers,
+});
 
 export default function Home() {
-  // const [data, setData] = useState([]);
+  const [data, setData] = useState([]);
 
-  // // Mengambil data saat komponen dimuat
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     try {
-  //       const response = await instance.get('/api/location');
-  //       setData(response.data?.data);
-  //     } catch (err) {
-  //       console.log(err);
-  //     }
-  //   };
+  // Mengambil data saat komponen dimuat
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await instance.get('/api/location');
+        setData(response.data?.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
 
-  //   fetchData();
-  // }, []);
+    fetchData();
+  }, []);
 
   return (
     <div>
       <h1>Hello World</h1>
-      {/* <Map3D data={data} />
-      <ToastNotify /> */}
+      <Map3D data={data} />
+      <ToastNotify />
     </div>
   );
 }
